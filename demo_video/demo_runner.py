6f5ab4a71d88f4dd7b074b7b038dea066da6805c
adam <adamz14@hotmail.com>
2025-01-26 10:40:06 +0200

    replaced hardcoded with args

--- demo_video/demo_runner.py
+++ demo_video/demo_runner.py
@@ -5,7 +5,6 @@ import tqdm
 from pathlib import Path
 from datetime import datetime
 
-
 def main(csv_file, output_path, config_file, base_dir):
 
     df = pd.read_csv(csv_file)
@@ -31,14 +30,25 @@ def main(csv_file, output_path, config_file, base_dir):
         try:
             # Run the command
             subprocess.run(command, check=True)
-            print("demo.py executed successfully.")
+            print(f"demo.py executed successfully for {file_name}.")
         except subprocess.CalledProcessError as e:
-            print(f"An error occurred while executing demo.py: {e}")
+            print(f"An error occurred while executing demo.py for {file_name}: {e}")
 
 if __name__ == "__main__":
-    csv_file = '/home/adam/Documents/Experiments/Mask2Former/Test on different clip, same video January23_2025/train_split.csv'
-    output_path = "/home/adam/PycharmProjects/Mask2Former/results"
-    config_file = "/home/adam/PycharmProjects/Mask2Former/configs/ichilov/video_maskformer2_R50_bs16_8ep.yaml"
-    base_dir = "/home/adam/mnt/qnap/annotation_data/data/sam2"
+    import argparse
 
-    main(csv_file, output_path, config_file, base_dir)
+    parser = argparse.ArgumentParser(description="Run demo.py on grouped video data.")
+    parser.add_argument("--csv_file", required=True, help="Path to the CSV file containing video data.")
+    parser.add_argument("--output_path", required=True, help="Base directory for output results.")
+    parser.add_argument("--config_file", required=True, help="Path to the configuration file.")
+    parser.add_argument("--base_dir", required=True, help="Base directory for input video frames.")
+
+    args = parser.parse_args()
+
+    main(args.csv_file, args.output_path, args.config_file, args.base_dir)
+
+
+    # --csv_file='/home/adam/Documents/Experiments/Mask2Former/Test on different clip, same video January23_2025/train_split.csv'
+    # --output_path="/home/adam/Documents/Experiments/Mask2Former/Test on different clip, same video January23_2025/results"
+    # --config_file="/home/adam/PycharmProjects/Mask2Former/configs/ichilov/video_maskformer2_R50_bs16_8ep.yaml"
+    # --base_dir="/home/adam/mnt/qnap/annotation_data/data/sam2"
